5c8438ab32187aced080ebe8eda126ac3cdc569e
Adam Edwards <chrono32123@gmail.com>
2022-10-18 11:17:44 -0500

    initial commit

---
+++ getPokemonImg.py
@@ -0,0 +1,71 @@
+import random
+import requests
+import os
+from PIL import Image
+
+##Get Pokemon Image Script
+##PURPOSE: Script to get Pokemon Name, Number, Official Art, and Generate Masked Art for stream overlays.
+##AUTHOR: chrono
+##LAST UPDATED: 10-17-2022
+##Be sure to run pip install -r requirements.txt BEFORE running this!
+
+##Image Cache Location (CHANGE THIS FOR YOUR SYSTEM)
+##propertyLoc = os.getcwd()
+propertyLoc = f"C:\streaming\Overlays\Pokemon Overlay\wtp"
+cache = f"{propertyLoc}/cache"
+imageLocation = ""
+
+##Generate random number out of total number of pokemon
+pokeNum = random.randrange(1,906)
+##pokeNum = 555
+
+##Gets pokemon info from pokeapi - this number is used to name the image and text files generated and cached.
+def getPokemonInfo():
+    apiUrl = f'https://pokeapi.co/api/v2/pokemon/{pokeNum}'
+    apiResponse = requests.get(apiUrl)
+
+    #Get Info from Response
+    pokeEntry = apiResponse.json()
+    pokeName = pokeEntry["name"]
+    pokeSprite = pokeEntry["sprites"]["other"]["official-artwork"]["front_default"]
+    cache_image(pokeSprite, cache)
+    name_to_file(pokeName, pokeNum)
+
+##Caches the image to your local storage using the cache dir specified above (lines 10 + 11)
+##If the image already exists then it will just return the path of the file.
+def cache_image(link, location):
+    filename = link.split("/")[-1]
+    cacheFolder = f"{location}/"
+    if not os.path.isfile(cacheFolder + filename):
+        r = requests.get(link)
+        with open(cacheFolder + filename, "wb") as f:
+            f.write(r.content)
+            f.close()
+        r.close()
+        hidePokemon(filename, cacheFolder)
+
+    return cacheFolder + filename
+    
+##output the pokemon name to a text file for reference (this could be improved but it was simple this way)
+def name_to_file(name, id):
+    filename = f"{cache}/{id}.txt"
+    if('-' in name):
+        name = name.split("-")[0]
+        
+    if not os.path.isfile(filename):
+        file = open(filename, "w")
+        file.write(name.upper())
+        file.close()
+
+##create a silhouette of the pokemon retrieved from pokeapi
+def hidePokemon(name, location):
+    filename = f"{location}/{name[:-4]}-masked.png"
+    if not os.path.isfile(filename):
+        im = Image.open(location + name)
+        alpha = im.getchannel('A')
+        masked = Image.new('RGBA', im.size, color=(42,42,134))
+        masked.putalpha(alpha)
+        masked.save(filename)
+
+getPokemonInfo()
+print(pokeNum) ##prints the national dex number for the pokemon so that MixItUp can read this value.
---
+++ readme.txt
@@ -0,0 +1,19 @@
+This python script was written to re-create Nutty's "Who's that Pokemon" ad break but using a different set of apps.
+The idea isn't original but hopefully this implementation is.
+
+BEFORE running this script for the first time:
+1. Install Python 3.10 (that's what version I used when I wrote it but older versions MIGHT work?)
+2. Install the necessary python dependencies using pip install -r requirements.txt
+
+If you're using MixItUp:
+    Be sure you import the commands correctly and setup the other assets you might need.
+    I have included the GIFs and still image I made of the background so that it would just work BUT you might need to make further adjustments.
+
+I'm offering this with very limited support. I won't walk you through installing MixItUp or Python. Google has the answers you need for that.
+
+If there's something in the code or MIU commands that could be better feel free to let me know.
+
+You can find me on these platforms:
+    YouTube: youtube.com/chrono32123
+    Twitter: twitter.com/chrono32123
+    Discord: Chrono
